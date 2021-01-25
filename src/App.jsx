c6247e3ada301145fe5475cc75076462923d8770
RatBoyJim <ratboyjim@gmail.com>
2021-01-25 20:27:13 +0000

    styling finished

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ function App() {
   return (
     <>
     <div className="movie-list-container">
-    <div className="movie-list">
-    <ReleaseBox></ReleaseBox>
-    <form action="https://www.imdb.com/calendar/?region=gb">
-      <input type="submit" value="View more upcoming releases"></input>
-    </form>
-    </div>
+      <div className="movie-list">
+        <ReleaseBox></ReleaseBox>
+          <form action="https://www.imdb.com/calendar/?region=gb">
+            <input type="submit" value="View more upcoming releases"></input>
+          </form>
+      </div>
     </div>
     </>
   );
